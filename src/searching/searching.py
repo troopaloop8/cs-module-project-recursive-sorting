e728616e643828a985df5cc6565c692c972b36e0
troopaloop8 <jontroop@gmail.com>
2020-08-14 02:39:05 +0900

    got searching working

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-    middle = (start+end)//2
-    if arr[middle] == target:
-        return middle
-    elif  target < arr[middle]:
-        middle = binary_search(arr,target,start,middle-1)
-    elif target > arr[middle]:
-        middle = binary_search(arr,target,middle+1,end)
-    return middle
+    #check base case of there being an item in list
+    if end >= start:
+        # if not base case find midpoint of list
+        mid = (end + start) // 2
+        # check value at midpoint to see if it is target
+        if arr[mid] == target:
+            #if so, return mid index as location of target
+            return mid
+        #if target is less than value at mid point, perform binary search on lower list
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+        #if targe is greater than value at mid point, perform binary search on higher list
+        else:
+            return binary_search(arr, target, mid + 1, end)
+    else:
+        # reached base case without finding element
+        return -1
 
 listy = [1,2,3,4,5,6,7,8,9]
 
